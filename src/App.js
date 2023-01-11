8c6ae079b951764258c083858131210ad8ed2166
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-01-11 23:04:11 +0530

    Added auth using metamask

--- src/App.js
+++ src/App.js
@@ -1,82 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import SocialLogin from "@biconomy/web3-auth";
-import { useCVPContext } from "./Context/CVPContext";
-import { useWeb3AuthContext } from "./Context/SocialLoginContext";
-import { useSmartAccountContext } from "./Context/SmartAccountContext";
-import Button from "./components/Button";
-import SingleTransaction from "./Context/SingleTransaction";
 
 function App() {
-	const {
-		address,
-		loading: eoaLoading,
-		userInfo,
-		connect,
-		disconnect,
-		getUserInfo,
-	} = useWeb3AuthContext();
-	const {
-		selectedAccount,
-		loading: scwLoading,
-		setSelectedAccount,
-	} = useSmartAccountContext();
-
-	console.log("address", address);
-
 	return (
 		<div>
-			<main>
-				<h1>Biconomy SDK React Web3Auth Example</h1>
-				<Button
-					onClickFunc={
-						!address
-							? connect
-							: () => {
-									setSelectedAccount(null);
-									disconnect();
-							  }
-					}
-					title={!address ? "Connect Wallet" : "Disconnect Wallet"}
-				/>
-
-				{eoaLoading && <h2>Loading EOA...</h2>}
-
-				{address && (
-					<div>
-						<h2>EOA Address</h2>
-						<p>{address}</p>
-					</div>
-				)}
-
-				{scwLoading && <h2>Loading Smart Account...</h2>}
-
-				{selectedAccount && address && (
-					<div>
-						<h2>Smart Account Address</h2>
-						<p>{selectedAccount.smartAccountAddress}</p>
-					</div>
-				)}
-
-				{address && (
-					<>
-						<Button
-							onClickFunc={() => getUserInfo()}
-							title="Get User Info"
-						/>
-						<SingleTransaction />
-					</>
-				)}
-
-				{userInfo && (
-					<div style={{ maxWidth: 800, wordBreak: "break-all" }}>
-						<h2>User Info</h2>
-						<pre style={{ whiteSpace: "pre-wrap" }}>
-							{JSON.stringify(userInfo, null, 2)}
-						</pre>
-					</div>
-				)}
-			</main>
+			<main></main>
 		</div>
 	);
 }
--- src/Context/constants.js
+++ src/Context/constants.js
@@ -1,13 +1,10 @@
-// import cvp from "../artifacts/contracts/Cvp.sol/Cvp.json";
-import cvp from "../artifacts/contracts/Temp.sol/Storage.json";
+import cvp from "../artifacts/contracts/Cvp.sol/Cvp.json";
 
-// export const CVPAddress = "0x0b81c4a0D021E2210F983BBe9b2aB67a34FAF455";
-export const CVPAddress = "0xE2bB2A2c6B5bC829322f1672788Ea85e1c188788";
-// export const CvpABI = cvp.abi;
+export const CVPAddress = "0x0b81c4a0D021E2210F983BBe9b2aB67a34FAF455";
 export const CvpABI = cvp.abi;
 
 export const ChainId = {
-	MAINNET: 1, // Ethereum
+	MAINNET: 1,
 	GOERLI: 5,
 	POLYGON_MUMBAI: 80001,
 	POLYGON_MAINNET: 137,
@@ -19,20 +16,3 @@ export const supportedChains = [
 	ChainId.POLYGON_MAINNET,
 	ChainId.POLYGON_MUMBAI,
 ];
-
-export const getRPCProvider = (chainId) => {
-	switch (chainId) {
-		case 1:
-			return "https://eth-mainnet.g.alchemy.com/v2/YMRFBPG1iyBwiRQIHThSWZanZj0NXUjv";
-		case 5:
-			return "https://eth-goerli.alchemyapi.io/v2/lmW2og_aq-OXWKYRoRu-X6Yl6wDQYt_2";
-		case 80001:
-			return "https://polygon-mumbai.g.alchemy.com/v2/Q4WqQVxhEEmBYREX22xfsS2-s5EXWD31";
-		case 137:
-			return "https://polygon-mainnet.g.alchemy.com/v2/6Tn--QDkp1vRBXzRV3Cc8fLXayr5Yoij";
-		default:
-			return "https://eth-mainnet.g.alchemy.com/v2/YMRFBPG1iyBwiRQIHThSWZanZj0NXUjv";
-	}
-};
-
-export const getSupportedChains = () => {};
