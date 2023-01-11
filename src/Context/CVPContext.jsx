1010ce380edf88e1afb65d2ae530107b25784859
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-01-11 23:29:51 +0530

    Linked some functions with contract

--- src/Context/CVPContext.jsx
+++ src/Context/CVPContext.jsx
@@ -31,5 +31,183 @@ export const useCVPContext = () => useContext(CVPContext);
 export const CVPProvider = ({ children }) => {
 	const { provider, signerAddress, signer } = useAuth();
 
-	return <CVPContext.Provider value={{}}>{children}</CVPContext.Provider>;
+	const connectingWithSmartContract = async () => {
+		fetchContract(signer);
+	};
+
+	const registerStudent = async (
+		name,
+		emailId,
+		address,
+		mobileNo,
+		studentId
+	) => {
+		const contract = fetchContract(signer);
+
+		let smartAccount = new SmartAccount(provider, options);
+		smartAccount = await smartAccount.init();
+
+		const data = contract.interface.encodeFunctionData("registerStudent", [
+			name,
+			studentId,
+			emailId,
+			mobileNo,
+		]);
+
+		const tx1 = {
+			to: CVPAddress,
+			data,
+		};
+
+		const txResponse = await smartAccount.sendGasLessTransaction({
+			transaction: tx1,
+		});
+		console.log(txResponse);
+	};
+
+	const registerStaff = async (name, department, emailId, level) => {
+		const contract = fetchContract(signer);
+
+		let smartAccount = new SmartAccount(provider, options);
+		smartAccount = await smartAccount.init();
+
+		const data = contract.interface.encodeFunctionData("registerStaff", [
+			name,
+			department,
+			emailId,
+			level,
+		]);
+
+		const tx1 = {
+			to: CVPAddress,
+			data,
+		};
+
+		const txResponse = await smartAccount.sendGasLessTransaction({
+			transaction: tx1,
+		});
+		console.log(txResponse);
+	};
+
+	const requestDocument = async (
+		docName,
+		description,
+		reqType,
+		department
+	) => {
+		const contract = fetchContract(signer);
+
+		let smartAccount = new SmartAccount(provider, options);
+		smartAccount = await smartAccount.init();
+
+		const data = contract.interface.encodeFunctionData("requestDocument", [
+			docName,
+			description,
+			reqType,
+			department,
+		]);
+
+		const tx1 = {
+			to: CVPAddress,
+			data,
+		};
+
+		const txResponse = await smartAccount.sendGasLessTransaction({
+			transaction: tx1,
+		});
+		console.log(txResponse);
+	};
+
+	const fetchAllRequestsForStudent = async () => {
+		const contract = fetchContract(signer);
+		const data = contract.fetchAllRequestsForStudent();
+		return data;
+	};
+
+	const fetchIndividualRequest = async (reqId) => {
+		const contract = fetchContract(signer);
+		const data = contract.fetchIndividualRequest(reqId);
+		return data;
+	};
+
+	const fetchAllDocumentsForStudent = async () => {
+		const contract = fetchContract(signer);
+		const data = contract.fetchAllDocumentsForStudent();
+		return data;
+	};
+
+	const fetchIndividualDocumentForStudent = async (docId) => {
+		const contract = fetchContract(signer);
+		const data = contract.fetchIndividualDocumentForStudent(docId);
+		return data;
+	};
+
+	const fetchAllRequestsForCollegeStaff = async () => {
+		const contract = fetchContract(signer);
+		const data = contract.fetchAllRequestsForCollegeStaff();
+		return data;
+	};
+
+	const rejectRequest = async (reqId, comment) => {
+		const contract = fetchContract(signer);
+
+		let smartAccount = new SmartAccount(provider, options);
+		smartAccount = await smartAccount.init();
+
+		const data = contract.interface.encodeFunctionData("rejectRequest", [
+			reqId,
+			comment,
+		]);
+
+		const tx1 = {
+			to: CVPAddress,
+			data,
+		};
+
+		const txResponse = await smartAccount.sendGasLessTransaction({
+			transaction: tx1,
+		});
+		console.log(txResponse);
+	};
+
+	const updateRequest = async (reqId, comment) => {
+		const contract = fetchContract(signer);
+
+		let smartAccount = new SmartAccount(provider, options);
+		smartAccount = await smartAccount.init();
+
+		const data = contract.interface.encodeFunctionData("updateRequest", [
+			reqId,
+			comment,
+		]);
+
+		const tx1 = {
+			to: CVPAddress,
+			data,
+		};
+
+		const txResponse = await smartAccount.sendGasLessTransaction({
+			transaction: tx1,
+		});
+		console.log(txResponse);
+	};
+
+	return (
+		<CVPContext.Provider
+			value={{
+				registerStudent,
+				registerStaff,
+				requestDocument,
+				fetchAllRequestsForStudent,
+				fetchIndividualRequest,
+				fetchAllDocumentsForStudent,
+				fetchIndividualDocumentForStudent,
+				fetchAllRequestsForCollegeStaff,
+				rejectRequest,
+				updateRequest,
+			}}
+		>
+			{children}
+		</CVPContext.Provider>
+	);
 };
