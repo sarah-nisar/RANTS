f49f6b337c8ad638d8385bdfd59fc9bf313b66c1
TanishSawant <tanishsawant@rediffmail.com>
2023-01-14 03:48:44 +0530

    firebase stuff

--- server/routes/register.route.js
+++ server/routes/register.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { registerController, otpController } = require("../controllers/register.controller");
+const upload = require('multer')();
+
+const { registerController, otpController, documentUploadController, getDocumentController, uploadToFirestoreController } = require("../controllers/register.controller");
 const router = express.Router();
 
 router.post("/", registerController);
 router.post("/otp", otpController);
-
+router.post("/documentUpload", upload.single("pdffile"),documentUploadController);
+router.get("/getDocument", getDocumentController);
+router.post("/toFirestore", upload.any(), uploadToFirestoreController);
 module.exports = router;
