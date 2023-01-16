0e281b8de3cda7af6fb893306b0a963b7f14954f
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-01-16 14:38:18 +0530

    Channged mailing system

--- server/controllers/register.controller.js
+++ server/controllers/register.controller.js
@@ -16,18 +16,19 @@ const upload = require("multer")();
 const streamifier = require("streamifier");
 
 const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrq2EpT_hNieQbs1GIgSkGXqGF-Fa7UQI",
-  authDomain: "centenary-1ac4e.firebaseapp.com",
-  databaseURL: "https://centenary-1ac4e-default-rtdb.firebaseio.com",
-  projectId: "centenary-1ac4e",
-  storageBucket: "centenary-1ac4e.appspot.com",
-  messagingSenderId: "647205891121",
-  appId: "1:647205891121:web:c19e6ed4340db83af5f910",
-  measurementId: "G-ZNZ83R972Y",
+	apiKey: "AIzaSyCrq2EpT_hNieQbs1GIgSkGXqGF-Fa7UQI",
+	authDomain: "centenary-1ac4e.firebaseapp.com",
+	databaseURL: "https://centenary-1ac4e-default-rtdb.firebaseio.com",
+	projectId: "centenary-1ac4e",
+	storageBucket: "centenary-1ac4e.appspot.com",
+	messagingSenderId: "647205891121",
+	appId: "1:647205891121:web:c19e6ed4340db83af5f910",
+	measurementId: "G-ZNZ83R972Y",
 };
 
 // Initialize Firebase
@@ -45,214 +46,191 @@ const db = getFirestore(app);
 // const analytics = getAnalytics(firebase);
 
 cloudinary.config({
-  cloud_name: "dwquo7ex8",
-  api_key: "536437728985795",
-  api_secret: "3XAmeZQ_bBFDvpt0Icq9Nk0QTlA",
+	cloud_name: "dwquo7ex8",
+	api_key: "536437728985795",
+	api_secret: "3XAmeZQ_bBFDvpt0Icq9Nk0QTlA",
 });
 
-sgMail.setApiKey(
-  //   "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"
-  // "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"
-  "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"
-);
+const mailConfig = {
+	host: "smtp.ethereal.email",
+	port: 587,
+	auth: {
+		user: "sherwood41@ethereal.email",
+		pass: "XntCS13BNQ59jTZuhZ",
+	},
+};
 
 // Validators
 const { validationResult } = require("express-validator");
 const secret = "authenticator.generateSecret()";
 let token;
-// const isValid2 = authenticator.check(token, secret);
-// console.log("isValidABOVE", isValid2);
 
 exports.registerController = (req, res) => {
-  //   console.log("yahoo");
-  const { email } = req.body;
-  const errors = validationResult(req);
+	const transporter = nodemailer.createTransport(mailConfig);
+	//   console.log("yahoo");
+	const { email } = req.body;
+	const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const firstError = errors.array().map((error) => error.msg)[0];
-    return res.status(400).json({
-      error: firstError,
-    });
-  } else {
-    // Alternative:
-    // Note: .generateSecret() is only available for authenticator and not totp/hotp
-    // console.log("secret", secret);
-    // console.log("token", token);
-    token = authenticator.generate(secret);
+	if (!errors.isEmpty()) {
+		const firstError = errors.array().map((error) => error.msg)[0];
+		return res.status(400).json({
+			error: firstError,
+		});
+	} else {
+		token = authenticator.generate(secret);
 
-    const temp = token;
-    const emailData = {
-      from: "appatil_b19@ce.vjti.ac.in",
-      to: email,
-      subject: "OTP for Email Verification",
-      html: `<h1>OTP to verify your email address is ${temp}</h1>`,
-    };
-    sgMail
-      .send(emailData)
-      .then((sent) => {
-        return res.status(200).json({
-          message: `Email has been sent to ${email}`,
-        });
-      })
-      .catch((err) => {
-        return res.status(400).json({
-          success: false,
-          error: "Could not send email\n" + err,
-        });
-      });
-  }
+		const temp = token;
+		const emailData = {
+			// from: "abjaiswal_b19@ce.vjti.ac.in",
+			from: "sherwood41@ethereal.email",
+			to: email,
+			subject: "OTP for Email Verification",
+			text: `OTP to verify your email address is ${temp}`,
+		};
+
+		transporter.sendMail(emailData).then((data, err) => {
+			console.log(data);
+			if (err) {
+				return res.status(400).json({
+					error: err,
+				});
+			}
+			return res.status(200).json({
+				data: data,
+				message: "Email sent successfully!",
+			});
+		});
+		// sgMail
+		// 	.send(emailData)
+		// 	.then((sent) => {
+		// 		return res.status(200).json({
+		// 			message: `Email has been sent to ${email}`,
+		// 		});
+		// 	})
+		// 	.catch((err) => {
+		// 		return res.status(400).json({
+		// 			success: false,
+		// 			error: "Could not send email\n" + err,
+		// 		});
+		// 	});
+	}
 };
 
 exports.otpController = (req, res) => {
-  const errors = validationResult(req);
+	const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const firstError = errors.array().map((error) => error.msg)[0];
-    return res.status(400).json({
-      error: firstError,
-    });
-  } else {
-    const { otp } = req.body;
-    // console.log("otp", otp);
-    try {
-      const isValid = true ? otp === '874603' : false;
-      console.log("isValid", isValid);
-      if (isValid) {
-        return res.status(200).json({
-          message: "OTP Valid",
-        });
-      } else {
-        return res.status(400).json({
-          message: "OTP InValid",
-        });
-      }
-      // or
-      // const isValid = authenticator.verify({ token, secret });
-    } catch (err) {
-      // Possible errors
-      // - options validation
-      // - "Invalid input - it is not base32 encoded string" (if thiry-two is used)
-      return res.status(400).json({
-        success: false,
-        error: "Some error\n" + err,
-      });
-    }
-  }
+	if (!errors.isEmpty()) {
+		const firstError = errors.array().map((error) => error.msg)[0];
+		return res.status(400).json({
+			error: firstError,
+		});
+	} else {
+		const { otp } = req.body;
+		try {
+			const isValid = true ? otp === token : false;
+			console.log("isValid", isValid);
+			if (isValid) {
+				return res.status(200).json({
+					message: "OTP Valid",
+				});
+			} else {
+				return res.status(400).json({
+					message: "OTP InValid",
+				});
+			}
+		} catch (err) {
+			return res.status(400).json({
+				success: false,
+				error: "Some error\n" + err,
+			});
+		}
+	}
 };
 
 exports.commentController = (req, res) => {
-  const errors = validationResult(req);
+	const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const firstError = errors.array().map((error) => error.msg)[0];
-    return res.status(400).json({
-      error: firstError,
-    });
-  } else {
-    const { comment, _reqId, _email } = req.body;
-    console.log("here", comment, _reqId, _email);
-    const emailData = {
-      from: "appatil_b19@ce.vjti.ac.in",
-      to: _email,
-      subject: "Document not issued",
-      html: `<h4>Sorry, we are unable to issue your document with request ID: ${_reqId}.
+	if (!errors.isEmpty()) {
+		const firstError = errors.array().map((error) => error.msg)[0];
+		return res.status(400).json({
+			error: firstError,
+		});
+	} else {
+		const { comment, _reqId, _email } = req.body;
+		console.log("here", comment, _reqId, _email);
+		const emailData = {
+			from: "appatil_b19@ce.vjti.ac.in",
+			to: _email,
+			subject: "Document not issued",
+			html: `<h4>Sorry, we are unable to issue your document with request ID: ${_reqId}.
       The following comment is stated by the issuer: ${comment}</h4>`,
-    };
-    sgMail
-      .send(emailData)
-      .then((sent) => {
-        return res.status(200).json({
-          message: `Email has been sent to ${_email}`,
-        });
-      })
-      .catch((err) => {
-        return res.status(400).json({
-          success: false,
-          error: "Could not send email\n" + err,
-        });
-      });
-  }
-};
-
-exports.documentUploadController = async (req, res) => {
-  let streamUpload = (req) => {
-    return new Promise((resolve, reject) => {
-      let stream = cloudinary.uploader.upload_stream((error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
-      });
-      console.log(file.buffer);
-      streamifier.createReadStream(file.buffer).pipe(stream);
-    });
-  };
-
-  async function upload(req) {
-    let result = await streamUpload(req);
-    console.log("$$$$$$$$$$$$$$$$$$$$$$");
-    console.log(result);
-  }
-
-  upload(req);
-  console.log("Done");
-  return res.status(200).json({
-    message: "Done!",
-  });
+		};
+		sgMail
+			.send(emailData)
+			.then((sent) => {
+				return res.status(200).json({
+					message: `Email has been sent to ${_email}`,
+				});
+			})
+			.catch((err) => {
+				return res.status(400).json({
+					success: false,
+					error: "Could not send email\n" + err,
+				});
+			});
+	}
 };
 
 exports.uploadToFirestoreController = async (req, res) => {
-  console.log(req.files);
-  console.log(req.body.docId);
-  console.log(req.formData);
-  if (!req.files) {
-    res.status(400).send("Error: No files found");
-  }
-  const formData = req.files[0];
-  const storageRef = ref(storage, `files/${formData.originalname}`);
-  const uploadTask = uploadBytesResumable(storageRef, formData.buffer);
-  // console.log("req", storageRef);
+	console.log(req.files);
+	console.log(req.body.docId);
+	console.log(req.formData);
+	if (!req.files) {
+		res.status(400).send("Error: No files found");
+	}
+	const formData = req.files[0];
+	const storageRef = ref(storage, `files/${formData.originalname}`);
+	const uploadTask = uploadBytesResumable(storageRef, formData.buffer);
 
-  try {
-    const downloadUrl = await getDownloadUrl(uploadTask.snapshot.ref);
-    console.log("-------------------------");
-    console.log(downloadUrl);
-    const docRef = doc(db, "Docs", req.body.docId);
-    console.log(docRef);
-    setDoc(docRef, { Url: downloadUrl });
+	try {
+		const downloadUrl = await getDownloadUrl(uploadTask.snapshot.ref);
+		console.log("-------------------------");
+		console.log(downloadUrl);
+		const docRef = doc(db, "Docs", req.body.docId);
+		console.log(docRef);
+		setDoc(docRef, { Url: downloadUrl });
 
-    console.log("Document written with ID: ", docRef.id);
+		console.log("Document written with ID: ", docRef.id);
 
-    return res.status(200).json({
-      message: "Reference document uploaded successfully",
-    });
-  } catch (err) {
-    return res.status(400).json({
-      success: false,
-      error: "Reference document could not be uploaded\n" + err,
-    });
-  }
+		return res.status(200).json({
+			message: "Reference document uploaded successfully",
+		});
+	} catch (err) {
+		return res.status(400).json({
+			success: false,
+			error: "Reference document could not be uploaded\n" + err,
+		});
+	}
 };
 
 exports.getDocumentController = async (req, res) => {
-  // const downloadUrl = await getDownloadUrl(uploadTask.snapshot.ref);
-  //   console.log("-------------------------");
-  //   console.log(downloadUrl);
-  console.log("req.body.docId", req.body.docId);
-  const docRef = doc(db, "Docs", req.body.docId);
-  // console.log(docRef);
-  const docSnap = await getDoc(docRef);
-  console.log("docSnap", docSnap.exists());
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-    return res.status(200).json({ data: docSnap.data() });
-  } else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-    return res.status(400).json({
-      success: false,
-      error: "No such document!",
-    });
-  }
+	// const downloadUrl = await getDownloadUrl(uploadTask.snapshot.ref);
+	//   console.log("-------------------------");
+	//   console.log(downloadUrl);
+	console.log("req.body.docId", req.body.docId);
+	const docRef = doc(db, "Docs", req.body.docId);
+	// console.log(docRef);
+	const docSnap = await getDoc(docRef);
+	console.log("docSnap", docSnap.exists());
+	if (docSnap.exists()) {
+		console.log("Document data:", docSnap.data());
+		return res.status(200).json({ data: docSnap.data() });
+	} else {
+		// doc.data() will be undefined in this case
+		console.log("No such document!");
+		return res.status(400).json({
+			success: false,
+			error: "No such document!",
+		});
+	}
 };
